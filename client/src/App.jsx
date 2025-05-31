65a2cef8318f9ca3623c5578d4fd78744902b66b
sanjaraiy <deepakrai78788@gmail.com>
2025-05-31 12:26:07 +0530

    added code reviewer with some extra package to better ui

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,45 +1,85 @@
-import { useState, useEffect } from 'react'
-import './App.css'
-import "prismjs/themes/prism-tomorrow.css"
+import { useState, useEffect } from "react";
+import "./App.css";
+import "prismjs/themes/prism-tomorrow.css";
 // import "prismjs/components/prism-jsx"
-import Prism from 'prismjs'
-import Editor from 'react-simple-code-editor'
+import Prism from "prismjs";
+import Editor from "react-simple-code-editor";
+import axios from "axios";
+import Markdown from "react-markdown";
+import rehypeHighlight from "rehype-highlight";
+import "highlight.js/styles/github-dark.css";
+import { DotLottieReact } from "@lottiefiles/dotlottie-react";
+
 function App() {
-  const [code, setCode] = useState('')
-  
+  const [code, setCode] = useState("");
+  const [review, setReview] = useState("");
+  const [loader, setLoader] = useState(false);
+
   useEffect(() => {
-       Prism.highlightAll()
-  }, [])
+    Prism.highlightAll();
+  }, []);
+
+  async function reviewCode() {
+    try {
+      setLoader(true);
+      const response = await axios.post(
+        `${import.meta.env.VITE_BACKEND_API_URL}/api/v1/ai/get-review`,
+        { code }
+      );
+
+      setReview(response.data);
+
+      console.log(response.data);
+
+      setLoader(false);
+    } catch (error) {
+      console.log(error.message);
+      alert(error.message);
+    } 
+  }
 
   return (
     <>
       <main>
-         <div className="left">
-           <div className="code">
+        <div className="left">
+          <div className="code">
             <Editor
-             value={code}
-             onValueChange={code => setCode(code)}
-             highlight={code => Prism.highlight(code, Prism.languages.javascript, 'javascript')}
-             padding={10}
-             style={
-              {
+              value={code}
+              onValueChange={(code) => setCode(code)}
+              highlight={(code) =>
+                Prism.highlight(code, Prism.languages.javascript, "javascript")
+              }
+              padding={10}
+              style={{
                 fontFamily: '"Fira code", "Fira Mono", monospace',
                 fontSize: 16,
-               
+
                 border: "1px solid #ddd",
                 borderRadius: "5px",
                 height: "100%",
-                width: "100%"
-              }
-             }
+                width: "100%",
+              }}
             ></Editor>
-           </div>
-           <div className="review">Review</div>
-         </div>
-         <div className="right"></div>
+          </div>
+          
+          <div className="review" onClick={reviewCode}>
+            Review
+          </div>
+        </div>
+        <div className="right">
+          {loader == false ? (
+            <Markdown rehypePlugins={[rehypeHighlight]}>{review}</Markdown>
+          ) : (
+            <DotLottieReact
+              src="https://lottie.host/b9c336d5-c863-4180-99f4-95a8befada2e/Kz8jPX320U.lottie"
+              loop
+              autoplay
+            />
+          )}
+        </div>
       </main>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
