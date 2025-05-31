7d6cdb4bc2a1e7e77bfea8f5c2f5459604fe9793
sanjaraiy <deepakrai78788@gmail.com>
2025-05-31 11:26:46 +0530

    added code editor with full functionality

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,9 +3,9 @@ import './App.css'
 import "prismjs/themes/prism-tomorrow.css"
 // import "prismjs/components/prism-jsx"
 import Prism from 'prismjs'
-
+import Editor from 'react-simple-code-editor'
 function App() {
-  const [count, setCount] = useState(0)
+  const [code, setCode] = useState('')
   
   useEffect(() => {
        Prism.highlightAll()
@@ -16,7 +16,23 @@ function App() {
       <main>
          <div className="left">
            <div className="code">
-            
+            <Editor
+             value={code}
+             onValueChange={code => setCode(code)}
+             highlight={code => Prism.highlight(code, Prism.languages.javascript, 'javascript')}
+             padding={10}
+             style={
+              {
+                fontFamily: '"Fira code", "Fira Mono", monospace',
+                fontSize: 16,
+               
+                border: "1px solid #ddd",
+                borderRadius: "5px",
+                height: "100%",
+                width: "100%"
+              }
+             }
+            ></Editor>
            </div>
            <div className="review">Review</div>
          </div>
